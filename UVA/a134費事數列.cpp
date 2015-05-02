08548a33dd82528b7f6aeafc0ddee44ae3488b3d
david30907d <davidtnfsh@gmail.com>
2015-05-03 00:30:02 +0800

    過了，要注意記憶體分段錯誤，不要讓他取到未知的陣列空間

--- UVA/a134費事數列.cpp
+++ UVA/a134費事數列.cpp
@@ -3,53 +3,58 @@
 #include<cstring>
 #include<stdio.h>
 using namespace std;
-int farray[30]={};
+int farray[40]={0};
 int counter=1;
 void fseries(){
 	farray[0]=1;
 	farray[1]=2;
-	while(counter<30){
+	while(counter<38){//ノㄓミ禣Α计 
 		farray[counter+1]=farray[counter]+farray[counter-1];
 		counter++;
 	}
 }
-int j=0;
+
 int main(){
 	int num=0;
-	fseries();
-	//printf("%d %d\n\n",farray[0],farray[1]);
+	fseries();//ミ计
 	while(scanf("%d",&num)!=EOF){
-		
 		for(int i=1;i<=num;i++){
+			int j=0;//ノㄓボ禣计计index 
 			int origininput;
 			scanf("%d",&origininput);
-			for(j=0;farray[j]<=origininput;j++){//тゑinput临璶禣计计礛搭 
-				//printf("%d ",farray[j]);	
+			for(j=0;farray[j]<=origininput&&j<=38;j++){//тゑinput临璶禣计计礛搭 
+					
 			}
-			j--;
-			//printf("\nfinal:%d ",farray[j]);
+			j--;			
 			
-			int answer=0;
+			int answer[50]={};
+			int index=0;
 			int input=origininput;
-			while(input>0){
-				if(farray[j]<input){
+			while(input>0&&j>=0){
+				if(farray[j]<input){//狦砆琘禣ㄆ计倒搭奔玥ê碞琌1 
 					input=input-farray[j];
+					answer[index++]=1;					
+					/*
 					answer=answer+1;
-					answer*=10;
+					answer*=10;*/
 				}
-				else if(farray[j]==input){
+				else if(farray[j]==input){//狦单琘禣ㄆ计玥–常琌0 
 					input=input-farray[j];
-					answer=answer+1;
-					for(int k=j;j>0;j--){
-						answer*=10;
+					answer[index++]=1;					
+					for(int k=j-1;k>=0;k--){
+						answer[index++]=0;
 					}
 				}
-				else{
-					answer*=10;
+				else{//狦input瞷ゑ禣ㄆ计临玥硂计碞恶箂 
+					answer[index++]=0;
 				}
 				j--;
 			}
-			printf("%d = %d (fib)\n",origininput,answer);
+			printf("%d = ",origininput);
+			for(int i=0;i<index&&i<50;i++){
+				printf("%d",answer[i]);	
+			}
+			printf(" (fib)\n");						
 		}
 	}	
  	return 0;
