9f0a8ad602b624a7e83fb628f22d0d36dd182d0b
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2015-10-19 23:12:55 +0800

    複習一下以前的code

--- zero judge/a010[因數分解].c
+++ zero judge/a010[因數分解].c
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]) {
 		int i,count=0;
 		for(i=2;i*i<=input;i++){
 			while(input%i==0){
+				//會計算有幾個這種的因式
+				//然後同步讓input變小
 				count++;
 				input=input/i;
 			}
@@ -28,6 +30,8 @@ int main(int argc, char *argv[]) {
 			count=0;
 		}
 		if(input>1){
+			//因為上面的判斷是指判斷到i^2 <= input
+			//所以如果剩下的數為質數，就沒辦法被除盡,所以要在這邊把最後一個質數給印出來
 			printf("%d\n",input);
 		}
 		
