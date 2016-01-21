d67134a1a3616a237aa552dd62d84405ecf83c98
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2016-01-21 17:03:04 +0800

    還沒解決

--- UVA/kmp.java
+++ UVA/kmp.java
@@ -9,10 +9,13 @@ public class Main{
 		Scanner cin=new Scanner(System.in);					
 		while(cin.hasNext()){					
 			String input=cin.nextLine();
-			failure(input);
+			String pattern = cin.nextLine();
+			failure(pattern);
+			
 			for(int i=0;i<=10;i++){
 				System.out.print(" "+failure[i]);
-			}
+			}			
+			Morris_Pratt(input, pattern);
 		}
 		cin.close();
 		
@@ -20,37 +23,30 @@ public class Main{
 	
 	// 目前比對成功的字串片段是p[0...i-1]
 	// 大幅挪動P之後，比對成功的部份剩下P[0...failure[i-1]]。	
-	void Morris_Pratt(string& t, string& p)
+	public static void Morris_Pratt(String t, String p)
 	{
-	    if (p.size() > t.size()) return;
+	    if (p.length() > t.length()) return;
 	 
 	    // 預先計算 P 的 failure function，O(P)。
-	    for (int i=1, j=failure[0]=-1; i<p.size(); ++i)
-	    {
-	        while (j >= 0 && p[j+1] != p[i])
-	            j = failure[j];
-	        if (p[j+1] == p[i]) j++;
-	        failure[i] = j;
-	    }
+	    failure(p);
 	 
 	    // 進行字串匹配，O(T)。
-	    for (int i=0, j=-1; i<t.size(); ++i)
-	//  for (int i=0, j=-1; i-j-1+p.size()<=t.size(); ++i)
+	    for (int i=0, j=-1; i<t.length(); ++i)
 	    {
 	        // 先試 p[0...j] 的「最長的相同前綴後綴」，
 	        // 再試「次長的相同前綴後綴」，
 	        // 再試「次次長的相同前綴後綴」……
 	        // 直到試成功為止。
-	        while (j >= 0 && p[j+1] != t[i])
-	            j = failure[j];
+	        while (j >= 0 && p.charAt(j+1) != t.charAt(i))
+	            j = -1;
 	 
 	        // t[i] 終於有用處了，終於可以加長！
-	        if (p[j+1] == t[i]) j++;
+	        if (p.charAt(j+1) == t.charAt(i)) j++;
 	 
 	        // 匹配到P！
-	        if (j == p.size()-1)
+	        if (j == p.length()-1)
 	        {
-	            cout << "P出現位置" << i - p.size() + 1;
+	            System.out.println("P出現位置" +(i - p.length() + 1)); 
 	 
 	            // 如果字串結尾不是'\0'的時候，就必須挪動 P。
 	            // 如果字串結尾是'\0'的時候，就能省略這一行。
@@ -71,7 +67,7 @@ public class Main{
 	        // 再試 p[0...failure[j]] 的「次長的相同前綴後綴」……
 	        // 直到試成功為止。
 	        while (j >= 0 && p.charAt(j+1) != p.charAt(i))
-	            j = failure[j];//
+	            j = -1;//
 	 
 	        // p[i] 終於有用處了，終於可以加長！
 	        if (p.charAt(j+1) == p.charAt(i)) j++;
