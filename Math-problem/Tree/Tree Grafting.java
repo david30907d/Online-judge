7a5b788e25b8fc1b50701367afb971e1b49c1021
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2016-01-22 15:50:31 +0800

    加註解

--- Math-problem/Tree/Tree Grafting.java
+++ Math-problem/Tree/Tree Grafting.java
@@ -23,9 +23,9 @@ public class Main{
   public static void convert(int ori_d, int atf_d){
     int child=0;
     while(input.charAt(i)=='d'){
-      child++;
+      child++;//child若繼續累加 代表現在tree走到他的兄弟了
       i++;
-      convert(ori_d+1, atf_d+child);
+      convert(ori_d+1, atf_d+child);//把森林修改成二元數的方式就是:对于节点x及其在树的转换前的父母y，level2(x) = level2(y) + 在树转换之前x作为孩子的序号
     }    
     i++;
     if(i>=input.length()) i--;//如果他已經到達自串的長度 就不能再++
